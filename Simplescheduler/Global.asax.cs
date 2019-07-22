2fb94d0e35d350cb845f1ad88a94e4a77eab6307
Swati Sharma <swati.sharma@asiasoftwaresolutions.com>
2019-07-22 11:56:52 +0530

    added log file added scheduler functionality

--- Simplescheduler/Global.asax.cs
+++ Simplescheduler/Global.asax.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Simplescheduler.Logger;
+using Simplescheduler.Models;
+using System;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,12 +9,18 @@ namespace Simplescheduler
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static int userId = 1;
+        ILogger logger = new TextLogger($"FPI_{userId}");
+
         protected void Application_Start()
         {
+            logger.Log("Application_Start.Time Taken = " + DateTime.UtcNow, LogType.Error);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            JobScheduler.Start();
         }
     }
 }
